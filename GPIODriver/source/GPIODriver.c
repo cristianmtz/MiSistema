3a8833054305ed19d530ea28a928de77d59c7160
Bryan Chavoya <bryan.chavoyaa@gmail.com>
2019-09-16 17:37:29 -0500

    PDDR DEFINIDOS PARA BOTONES Y  DISPLAYS, creacion de funciones para Pines y para salidas

--- GPIODriver/source/GPIODriver.c
+++ GPIODriver/source/GPIODriver.c
@@ -57,22 +57,105 @@
 /****** Local Variables *******/
 
 
+
+
+
 /****** Local Functions *******/
 
-/** @brief
- *  @param
- *  @param
- *  @return
- */
+/** @brief This function is to configure System Clock Gate 5
+  *  @return void
+  */
+  static void SCGC5_Configuration (void)
+  {
+	  volatile uint32_t *pdwSCGC5 = (volatile uint32_t*)0x40048038;
+	  *pdwSCGC5 |= (1<<10);
+  }
 
-/*
- * static void System_Clock_Gating (void)
- * {
- * 		volatile uint32_t* pdwSCGC5 = (volatile uint32_t*)0x40048038;
- * 		*pdwSCGC5 |= (1<<10);
- *
- * }
- */
+
+ /** @brief This function sets all the Port Output Registers (not including the LED PDOR)
+  *  @return void
+  */
+
+  /*void Port_Output_Registers (void)
+  {
+
+  }
+  */
+
+
+
+ /** @brief his function is to configure the pins (PCR, PDDR)
+  *  @return void
+  */
+  static void Pin_Configurations (void)
+  {
+	/*LED CONFIG*/
+  	volatile uint32_t *pdwGPIO_PDDR_RED_LED = (volatile uint32_t*)0x400FF054;
+    volatile uint32_t *pdwMUX_RED_LED = (volatile uint32_t*)0x4004A048;
+    *pdwMUX_RED_LED |= (1<<8);
+    *pdwGPIO_PDDR_RED_LED |= (1<<18);
+
+
+    /*BUTTON PDDR*/
+    volatile uint32_t *pdwGPIO_PDDR_BUTTON_1 = (volatile uint32_t*)0x400FF094; //PTC3
+    *pdwGPIO_PDDR_BUTTON_1 |=(1<<3);
+    volatile uint32_t *pdwGPIO_PDDR_BUTTON_2 = (volatile uint32_t*)0x400FF054; //PTB3
+    *pdwGPIO_PDDR_BUTTON_2 |=(1<<3);
+    volatile uint32_t *pdwGPIO_PDDR_BUTTON_3 = (volatile uint32_t*)0x400FF054; //PTB16
+    *pdwGPIO_PDDR_BUTTON_3 |=(1<<16);
+    volatile uint32_t *pdwGPIO_PDDR_BUTTON_4 = (volatile uint32_t*)0x400FF054; //PTB17
+    *pdwGPIO_PDDR_BUTTON_4 |=(1<<17);
+    volatile uint32_t *pdwGPIO_PDDR_BUTTON_5 = (volatile uint32_t*)0x400FF094; //PTC1
+    *pdwGPIO_PDDR_BUTTON_5 |=(1<<1);
+    volatile uint32_t *pdwGPIO_PDDR_BUTTON_6 = (volatile uint32_t*)0x400FF094; //PTC2
+    *pdwGPIO_PDDR_BUTTON_6 |=(1<<2);
+
+    /*BUTTON PCR*/
+    volatile uint32_t *pdwPE_BUTTON_1 = (volatile uint32_t*)0x4004B00C; //PTC3
+    *pdwPE_BUTTON_1 |=(1<<1);
+    volatile uint32_t *pdwPE_BUTTON_2 = (volatile uint32_t*)0x4004A00C; //PTB3
+    *pdwPE_BUTTON_2 |=(1<<1);
+    volatile uint32_t *pdwPE_BUTTON_3 = (volatile uint32_t*)0x4004A040; //PTB16
+    *pdwPE_BUTTON_3 |=(1<<1);
+    volatile uint32_t *pdwPE_BUTTON_4 = (volatile uint32_t*)0x4004A044; //PTB17
+    *pdwPE_BUTTON_4 |=(1<<1);
+    volatile uint32_t *pdwPE_BUTTON_5 = (volatile uint32_t*)0x4004B004; //PTC1
+    *pdwPE_BUTTON_5 |=(1<<1);
+    volatile uint32_t *pdwPE_BUTTON_6 = (volatile uint32_t*)0x4004B008; //PTC2
+    *pdwPE_BUTTON_6 |=(1<<1);
+
+
+    /*DISPLAYS SEGMENTS PDDR*/
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_SEGMENT_1 = (volatile uint32_t*)0x400FF054; //PTB2
+    *pdwGPIO_PDDR_DISPLAY_SEGMENT_1 |=(1<<2);
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_SEGMENT_2 = (volatile uint32_t*)0x400FF0D4; //PTD0
+    *pdwGPIO_PDDR_DISPLAY_SEGMENT_2 |=(1<<0);
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_SEGMENT_3 = (volatile uint32_t*)0x400FF0D4; //PTD1
+    *pdwGPIO_PDDR_DISPLAY_SEGMENT_3 |=(1<<1);
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_SEGMENT_4 = (volatile uint32_t*)0x400FF0D4; //PTD2
+    *pdwGPIO_PDDR_DISPLAY_SEGMENT_4 |=(1<<2);
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_SEGMENT_5 = (volatile uint32_t*)0x400FF0D4; //PTD3
+    *pdwGPIO_PDDR_DISPLAY_SEGMENT_5 |=(1<<3);
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_SEGMENT_6 = (volatile uint32_t*)0x400FF0D4; //PTD4
+    *pdwGPIO_PDDR_DISPLAY_SEGMENT_6 |=(1<<4);
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_SEGMENT_7 = (volatile uint32_t*)0x400FF0D4; //PTD5
+    *pdwGPIO_PDDR_DISPLAY_SEGMENT_7 |=(1<<5);
+
+    /*DISPLAYS SEGMENTS PCR*/
+
+    /*DISPLAY PDDR*/
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_1 = (volatile uint32_t*)0x400FF094; //PTC10
+    *pdwGPIO_PDDR_DISPLAY_1 |= (1<<10);
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_2 = (volatile uint32_t*)0x400FF094; //PTC11
+    *pdwGPIO_PDDR_DISPLAY_2 |= (1<<11);
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_3 = (volatile uint32_t*)0x400FF114; //PTE0
+    *pdwGPIO_PDDR_DISPLAY_3 |= (1<<0);
+    volatile uint32_t *pdwGPIO_PDDR_DISPLAY_4 = (volatile uint32_t*)0x400FF114; //PTE1
+    *pdwGPIO_PDDR_DISPLAY_4 |= (1<<1);
+
+    /*DISPLAY PCR*/
+  }
+
 
 /****** Global Functions *******/
 
@@ -88,23 +171,16 @@
  */
 int main(void)
 {
-  	/* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
-  	/* Init FSL debug console. */
     BOARD_InitDebugConsole();
-
-    volatile uint32_t* pdwSCGC5 = (volatile uint32_t*)0x40048038; //función System Clock Gate
-    volatile uint32_t* pdwGPIO_PDDR = (volatile uint32_t*)0x400FF054;
-    volatile uint32_t *pdwMUX = (volatile uint32_t*)0x4004A048;
-    volatile uint32_t* pdwPDOR = (volatile uint32_t*)0x400FF040;
+    volatile uint32_t* pdwPDOR_RED_LED = (volatile uint32_t*)0x400FF040;
     uint32_t dwdelay = 0;
 
-
-      *pdwSCGC5 |= (1<<10);
-      *pdwMUX |= (1<<8);
-      *pdwGPIO_PDDR |= (1<<18);
+      SCGC5_Configuration();
+      Pin_Configurations();
+      //Port_Output_Registers();
 
         while(1)
         {
@@ -114,8 +190,7 @@ int main(void)
         	{
 
         	}
-
-        	*pdwPDOR ^= (1<<18);
+        	*pdwPDOR_RED_LED ^= (1<<18);
 
         }
         return 0 ;
